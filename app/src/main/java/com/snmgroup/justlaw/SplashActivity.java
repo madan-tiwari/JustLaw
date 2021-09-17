485a953182ce6136761d41a11e021b3843e5c0c2
sudarshantiwari <tiwarisudarshan21@gmail.com>
2021-09-17 22:54:16 +0545

    splash basic test

---
+++ app/src/main/java/com/snmgroup/justlaw/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.snmgroup.justlaw;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+ Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        btn1 = findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
